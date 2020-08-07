784789632693b57c85b2e7804d3419371c1407de
saritaborad <borad.sarita@gmail.com>
2020-08-07 19:39:00 +0530

    added comment and gitignor

--- tweet/admin.py
+++ tweet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tweet,TweetLike,Profile
+from .models import Tweet,TweetLike,Profile,Comment
 
 # Register your models here.
 class TweetLikeAdmin(admin.TabularInline):
@@ -14,3 +14,4 @@ class TweetAdmin(admin.ModelAdmin):
 
 admin.site.register(Tweet,TweetAdmin)
 admin.site.register(Profile)
+admin.site.register(Comment)
